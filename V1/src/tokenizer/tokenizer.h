aa0c3a3eceea235ed3faab9aa7732b54beedb51a
Sahil Raj <havoksahil@gmail.com>
2024-10-23 00:39:30 +0530

    V1: (tokenizer complete: with tests)

--- V1/src/tokenizer/tokenizer.h
+++ V1/src/tokenizer/tokenizer.h
@@ -1,6 +1,6 @@
 /******************************************************
  *	Tokenizer
- *
+ *	--------------------------------------------------
  *	This section contains the declaration of data-
  *	structures and functions for string tokenizer.
  *
@@ -27,17 +27,30 @@
 #include <err_codes.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
+
 
 /*----------------------------------------
  *	Types and definations for Tokenizer
  *----------------------------------------*/
 
-#define _TOK_TYPE_COMMENT 	0xF1	/*   */
-#define _TOK_TYPE_LABEL 		0xF2	/*   */
-#define _TOK_TYPE_MNENOMIC 	0xF3	/*   */
-#define _TOK_TYPE_OPERATOR	0xF4	/*   */
-#define _TOK_TYPE_DIRECTIVE	0xF5	/*   */
-#define _TOK_TYPE_BLANK			0xF6	/*   */
+#define _TOK_CARGO_PKT_WIN 	1000	/* Packet Window Size for Loading the File Content into Cargo  */
+#define _TOK_LINE_BUFF_SZ		1024	/* Buffer Size for Line Reading   */
+#define _TOK_IF_MAX_THREAD	50		/* The maximum number of threads for Jarification  */
+#define _TOK_TOKEN_COMMENT_SIZE 	256
+#define _TOK_TOKEN_LABEL_SIZE 		32
+#define _TOK_TOKEN_MNEMONIC_SIZE 	32
+#define _TOK_TOKEN_OPERAND_SIZE 	32
+
+#define _TOK_TYPE_COMMENT 	0xF1	/* Token type Comment  */
+#define _TOK_TYPE_LABEL 		0xF2	/* Token type Label  */
+#define _TOK_TYPE_WORD	 		0xF3	/* Token type Mnemonic  */
+#define _TOK_TYPE_OPERATOR	0xF4	/* Token type Operator  */
+#define _TOK_TYPE_DIRECTIVE	0xF5	/* Token type Directive  */
+#define _TOK_TYPE_BLANK			0xF6	/* Token type Black  */
+
+#define _TOK_LINE_UNSET			0			/* Unset value of line number of token or packet */
+#define _TOK_COL_UNSET			0			/* Unset value of column number of token  */
 
 /*----------------------------------------
  *	Data structures for Tokenizer
@@ -45,9 +58,11 @@
 
 /* The structure for tokens  */
 struct tk_token {
-	AType type;								/*   */
-	AString token;						/*   */
-	struct tk_token*  next;		/*   */
+	AType type;								/* Type of Token  */
+	ASize lno;								/* The line number of token  */
+	ASize cno;								/* The column number of token  */
+	AString token;						/* Token Content  */
+	struct tk_token*  next;		/* Next Item in Linked List of Token  */
 
 	void (*destroy)(struct tk_token*);
 };
@@ -56,8 +71,9 @@ typedef struct tk_token Token;
 
 /* The Structure for Jar/List of Tokens for a line.  */
 struct tk_jar {
-	ASize size;								/*   */
-	Token* tokens;						/*   */
+	ASize size;								/* Size of Jar [Number of Tokens in Jar]  */
+	Token* tokens;						/* Tokens Linked List  */
+	ASize lno;
 
 	void (*destroy)(struct tk_jar*);
 	AErr (*put)(struct tk_jar*, Token*);
@@ -69,9 +85,10 @@ typedef struct tk_jar Jar;
 
 /* The structure for packets in the cargo  */
 struct tk_packet {
-	void* content;						/*   */
-	struct tk_packet* prev;		/*   */
-	struct tk_packet* next;		/*   */
+	void* content;						/* The Content of the Packet.  */
+	ASize lno;									/* The line number of the packet  */
+	struct tk_packet* prev;		/* The Previous Item in the Packet Queue. */
+	struct tk_packet* next;		/* The Next item in the Packet Queue. */
 	
 	void (*destroy)(struct tk_packet*, ABool);
 	AString (*get)(struct tk_packet*);
@@ -82,16 +99,16 @@ typedef struct tk_packet Packet;
 
 /* The structure for Cargo/List of Jar/List of for complete file */
 struct tk_cargo {
-	ASize size;	/* Reflects the number of packet in the Cargo   */
-	ASize crate_num;					/*   */
-	ASize crate_size;					/*   */
-	Packet* front;						/*   */
-	Packet* back;							/*   */
-	AType status;							/*   */
+	ASize size;								/* Reflects the number of packet in the Cargo   */
+	ASize crate_num;					/* The Number of Crates in the Cargo  */
+	ASize crate_size;					/* The Crate Size: Number of Packets in each Crate.  */
+	Packet* front;						/* The Front of the Packet Queue.  */
+	Packet* back;							/* The Back of the Packet Queue.   */
+	AType status;							/* The Status of the Cargo.  */
 
 	void (*destroy)(struct tk_cargo*, ABool);
 	void* (*get)(struct tk_cargo*, ASize);
-	AErr (*load)(struct tk_cargo*, void*);
+	AErr (*load)(struct tk_cargo*, ASize, void*);
 };
 
 typedef struct tk_cargo Cargo;
@@ -99,20 +116,22 @@ typedef struct tk_cargo Cargo;
 /**
  * The Main Structure of Tokenizer 
  * ------------------------------*/
+
 struct tk_tokenizer_interface {
-	FILE* file;
-	Cargo* cargo;
-	ASize file_size;
-	ASize current_pos;
-	ASize num_workers;
-	AType status;
+	FILE* file;						/* The File Descriptor of the Tokenizer Interface. */
+	Cargo* cargo;					/* The Cargo Pointer. */
+	ASize file_size;			/* The Total File Size.  */
+	ASize current_pos;		/* The Current Position of the Tokenizer Interface.  */
+	ASize num_workers;		/* The Number of Workers in the Tokenizer Interface  */
+	AType status;					/* The Status of the Tokenizer Interface.  */
 	
 	void (*destroy)(struct tk_tokenizer_interface*);
-	AErr (*fillCargo)(Cargo*);
-	AInt (*percentageWorkDone)(void);
+	AErr (*fillCargo)(struct tk_tokenizer_interface*, Cargo*);
+	AInt (*percentageWorkDone)(struct tk_tokenizer_interface*);
 };
 
 typedef struct tk_tokenizer_interface TokInterface;
+
 
 /*----------------------------------------
  *	Main Functions for Tokenizer
@@ -129,31 +148,18 @@ TokInterface* tk_new_TokInterface(FILE*, ASize);
 /**
  * Functions for Token Data Structure
  * ---------------------------------------*/
-Token* tk_new_Token(AType, AString);				/*   */
-void tk_destroy_Token(Token*);							/*   */
-
+Token* tk_new_Token(AType, AString);				/* Allocate New Token in Heap.  */
 /**
  * Functions for Jar Data Structure
  * ---------------------------------------*/
-Jar* tk_new_Jar();													/*   */
-void tk_destroy_Jar(Jar*);									/*   */
-AErr tk_Jar_put(Jar*, Token*);							/*   */
-Token* get(const Jar*, ASize);							/*   */
-
+Jar* tk_new_Jar();													/* Allocate New Jar in Memory.  */
 /**
  * Functions for Cargo Data Structure
  * ---------------------------------------*/
-Cargo* tk_new_Cargo();											/*   */
-void tk_destroy_Cargo(Cargo*, ABool);				/*   */
-AErr tk_Cargo_load(Cargo*, void*);					/*   */
-void* tk_Cargo_get(const Cargo*, ASize);		/*   */
-
+Cargo* tk_new_Cargo();											/* Allocate New Cargo in Memory.  */
 /**
  * Functions for Packet Data Structure
  * ---------------------------------------*/
-Packet* tk_new_Packet();										/*   */
-void tk_destroy_Packet(Packet*, ABool);			/*   */
-AString tk_Packet_get(const Packet*);				/*   */
-
+Packet* tk_new_Packet(void*, ASize);				/* Allocate New Packet in Memory.  */
 
 #endif
--- V1/tests/test_tokenizer.c
+++ V1/tests/test_tokenizer.c
@@ -1,4 +1,5 @@
 #include "tokenizer.h"
+#include <time.h>
 
 #define SUCCESS 0
 #define FAILURE 1
@@ -11,17 +12,14 @@ int test_cargo() {
     AString strings[] = {"Hello", "World", "I", "am", "Assembler"};
     int i;
     for (i = 0; i<5; i++) {
-        if (cargo->load(cargo, strings[i]) != SUCCESS) {
-            printf("Loading Failed.\n");
+        if (cargo->load(cargo, i, strings[i]) != SUCCESS) {
             return FAILURE;
         }
     }
 
     for (i = 0; i<5; i++) {
         AString string = cargo->get(cargo, i);
-        printf("The %dth element is %s\n", i, string);
     }
-    printf("The size of cargo after loading is %d\n", cargo->size);
     cargo->destroy(cargo, FALSE);
     cargo = NULL;
 
@@ -47,9 +45,60 @@ int test_Jar() {
         Token* token = jar->get(jar, i);
         if (token == NULL)
             return FAILURE;
-        printf("The token is %s with type: %d\n", token->token, token->type);
     }
-    printf("The size of jar after insertion is %d\n", jar->size);
+
+    return SUCCESS;
+}
+
+int test_tokenizer_interface() {
+    FILE* file = fopen("hello.txt", "r");
+    if (file == NULL)
+        return FAILURE;
+    TokInterface* ti = tk_new_TokInterface(file, 20);
+    if (ti == NULL)
+        return FAILURE;
+
+    Cargo* cargo = tk_new_Cargo();
+    if (cargo == NULL)
+        return FAILURE;
+
+    double start = clock();
+    while (ti->status == 0) {
+        if (ti->fillCargo(ti, cargo) != SUCCESS)
+            return FAILURE;
+
+        printf("The size of cargo is %d\n", ti->cargo->size);
+        printf("The status is %s.\n", (ti->status == 1)? "Completed": "Remaining");
+        printf("The percentage of work done is %d\n", ti->percentageWorkDone(ti));
+    }
+    double end = clock();
+
+    ASize i;
+    for (i = 0; i<cargo->size; i++) {
+        Jar* jar = cargo->get(cargo, i);
+        if (jar == NULL)
+            return FAILURE;
+        ASize jarSize = jar->size;
+        ASize j;
+        printf("Jar(%d) [Size: %d]\n", jar->lno, jar->size);
+        for (j = 0; j<jarSize; j++) {
+            Token* token = jar->get(jar, j);
+            if (token == NULL)
+                return FAILURE;
+
+            if (token->cno != _TOK_COL_UNSET)
+                printf("%s (col:%d, type: %d), ", token->token, token->cno, token->type);
+        }
+        printf("\n\n");
+    }
+
+    fclose(file);
+    ti->destroy(ti);
+    cargo->destroy(cargo, TRUE);
+
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("The time taken to complete code is %f\n", cpu_time_used);
+    printf("Memory Freed.\n");
     return SUCCESS;
 }
 
@@ -57,6 +106,8 @@ int main() {
     if (test_cargo() != SUCCESS)
         return FAILURE;
     if (test_Jar() != SUCCESS)
+        return FAILURE;
+    if (test_tokenizer_interface() != SUCCESS)
         return FAILURE;
     return SUCCESS;
 }
